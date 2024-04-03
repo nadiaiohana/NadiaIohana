0f6b1757afc7894d450fef13d2690d1eca032e10
nadiaiohana <130075110+nadiaiohana@users.noreply.github.com>
2024-04-03 09:11:06 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ for(let i=0;i <botoes.length;i++){
 }
 const contadores=document.querySelectorAll(".contador")
 const tempoObjetivo1= new Date("2024-12-25T00:00:00);
-const tempoObjetivo1= new Date("2024-12-25T00:00:00);
+const tempoObjetivo1= new Date("2024-04-25T00:00:00);
 const tempoObjetivo1= new Date("2024-12-25T00:00:00);
 const tempoObjetivo1= new Date("2024-12-25T00:00:00);                               
 let tempoAtual= new Date();                               
